6e9106645bfea71a03a64d89f4e036ccd539ff2f
Pablo Machado <100440808+pmmachadov@users.noreply.github.com>
2024-05-23 14:30:44 +0200

    Comments deleted

--- src/controllers/quizController.js
+++ src/controllers/quizController.js
@@ -7,19 +7,16 @@ const {
     deleteQuestion
 } = require('../models/question');
 
-// Create a new question
 exports.createQuestion = (req, res) => {
     const newQuestion = createQuestion(req.body);
     res.status(201).json(newQuestion);
 };
 
-// Get all questions
 exports.getAllQuestions = (req, res) => {
     const questions = getAllQuestions();
     res.status(200).json(questions);
 };
 
-// Get a question by ID
 exports.getQuestionById = (req, res) => {
     const question = getQuestionById(req.params.id);
     if (question) {
@@ -29,13 +26,11 @@ exports.getQuestionById = (req, res) => {
     }
 };
 
-// Get questions by student ID
 exports.getQuestionsByStudentId = (req, res) => {
     const questions = getQuestionsByStudentId(req.params.student_id);
     res.status(200).json(questions);
 };
 
-// Update a question
 exports.updateQuestion = (req, res) => {
     const updatedQuestion = updateQuestion(req.params.id, req.body);
     if (updatedQuestion) {
@@ -45,7 +40,6 @@ exports.updateQuestion = (req, res) => {
     }
 };
 
-// Delete a question
 exports.deleteQuestion = (req, res) => {
     const deletedQuestion = deleteQuestion(req.params.id);
     if (deletedQuestion) {
