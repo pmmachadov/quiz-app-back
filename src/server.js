9f8db429161f8380ebcef3ff23efd4d0ea02d4e3
Pablo Machado <100440808+pmmachadov@users.noreply.github.com>
2024-05-24 11:33:19 +0200

    Implemented API endpoints for teachers to review Q&A. Added topicQuestionsController and routes, integrated with server.js, and created topicQuestionsData.json.

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const quizRoutes = require('./routes/quizRoutes');
 const userRoutes = require('./routes/userRoutes');
 const topicRoutes = require('./routes/topicRoutes');
 const qnaRoutes = require('./routes/qnaRoutes');
+const topicQuestionsRoutes = require('./routes/topicQuestionsRoutes');
 
 app.use(express.json());
 
@@ -11,6 +12,7 @@ app.use('/api/quiz', quizRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/topic', topicRoutes);
 app.use('/api/answers', qnaRoutes);
+app.use('/api/topic-questions', topicQuestionsRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
