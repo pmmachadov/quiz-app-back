3e46764761a4c85002cda33970a1819a756443bf
Pablo Machado <100440808+pmmachadov@users.noreply.github.com>
2024-05-24 22:13:57 +0200

    feat: Add getAllQuestions and getQuestionById endpoints
    The code changes implement the `getAllQuestions` and `getQuestionById` endpoints in the `topicQuestionsController`.
    These endpoints allow retrieving all questions and a specific question by ID from the `topicQuestionsData.json` file.
    The routes in `topicQuestionsRoutes.js` have been updated to use these new controller functions.

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const path = require('path');
+const cors = require('cors');
 const app = express();
+
 const quizRoutes = require('./routes/quizRoutes');
 const userRoutes = require('./routes/userRoutes');
 const topicRoutes = require('./routes/topicRoutes');
 const qnaRoutes = require('./routes/qnaRoutes');
 const topicQuestionsRoutes = require('./routes/topicQuestionsRoutes');
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/quiz', quizRoutes);
@@ -13,6 +17,16 @@ app.use('/api/user', userRoutes);
 app.use('/api/topic', topicRoutes);
 app.use('/api/answers', qnaRoutes);
 app.use('/api/topic-questions', topicQuestionsRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+    const frontEndPath = path.join(__dirname, '../../quiz-app-front-end/dist');
+
+    app.use(express.static(frontEndPath));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(frontEndPath, 'index.html'));
+    });
+}
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
