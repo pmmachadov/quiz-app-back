becbc478f4af865a094f5c0fbd3c7e47aa380259
3liezerDev <3dgar3liezer@gmail.com>
2024-05-30 23:29:28 +0200

    resolving functionality issues in the back end, refactoring code of other functionalities.

--- src/server.js
+++ src/server.js
@@ -4,15 +4,19 @@ const cors = require('cors');
 const app = express();
 require('dotenv').config();
 
+// Importa las rutas
 const userRoutes = require('./routes/userRoutes');
 const topicRoutes = require('./routes/topicRoutes');
 
+// Configuración de middlewares
 app.use(cors());
 app.use(express.json());
 
+// Usa las rutas
 app.use('/api/user', userRoutes);
 app.use('/api', topicRoutes);
 
+// Configuración para producción
 if (process.env.NODE_ENV === 'production') {
     const frontEndPath = path.join(__dirname, '../../quiz-app-front-end/dist');
     app.use(express.static(frontEndPath));
@@ -21,6 +25,7 @@ if (process.env.NODE_ENV === 'production') {
     });
 }
 
+// Puerto de la aplicación
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
